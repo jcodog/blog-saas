2a0ed7519e5bcacb808c9540df9a5f5fe298b645
Jason Connolly <jcodog@jconet.co.uk>
2025-07-14 19:24:41 +0100

    Initialise shadcn

--- src/app/components/providers.tsx
+++ src/app/components/providers.tsx
@@ -1,28 +1,32 @@
-"use client"
+"use client";
 
 import {
-  QueryCache,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query"
-import { HTTPException } from "hono/http-exception"
-import { PropsWithChildren, useState } from "react"
+	QueryCache,
+	QueryClient,
+	QueryClientProvider,
+} from "@tanstack/react-query";
+import { HTTPException } from "hono/http-exception";
+import { PropsWithChildren, useState } from "react";
+import { toast } from "sonner";
 
 export const Providers = ({ children }: PropsWithChildren) => {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        queryCache: new QueryCache({
-          onError: (err) => {
-            if (err instanceof HTTPException) {
-              // global error handling, e.g. toast notification ...
-            }
-          },
-        }),
-      })
-  )
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				queryCache: new QueryCache({
+					onError: (err) => {
+						if (err instanceof HTTPException) {
+							// global error handling, e.g. toast notification ...
+							toast.error(`[${err.status}] ${err.message}`);
+						}
+					},
+				}),
+			})
+	);
 
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  )
-}
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+		</QueryClientProvider>
+	);
+};
