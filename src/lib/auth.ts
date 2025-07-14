9ae7ccfe15b70db140b41550bbfd64608adb16df
Jason Connolly <jcodog@jconet.co.uk>
2025-07-14 20:39:54 +0100

    Implement admin plugin for better-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,12 @@
 import { loadDbClient } from "@/lib/db";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { admin } from "better-auth/plugins";
 
 const prisma = loadDbClient({ database_url: process.env.DATABASE_URL! });
 
 export const auth = betterAuth({
+	plugins: [admin()],
 	database: prismaAdapter(prisma, {
 		provider: "postgresql",
 	}),
